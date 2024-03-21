a24d8c0fea01cd2f6a5f634440cd1bb38b52c341
Code-Y-Learner <jktmdwo@naver.com>
2024-03-22 05:42:30 +0900

    원본으로 복귀

--- app/api/mysql/connect.js
+++ app/api/mysql/connect.js
@@ -1,5 +1,4 @@
 import mysql from 'mysql2/promise';
-import redis from 'redis';
 
 export default async function connection() {
 	const connection = await mysql.createConnection({
@@ -8,15 +7,6 @@ export default async function connection() {
 		password: process.env.MYSQL_PW,
 		database: process.env.MYSQL_DB,
 	});
-	console.log('MySQL connected.');
-
-	const cache = redis.createClient({
-		host : "aurora-redis.z6wkin.ng.0001.apne1.cache.amazonaws.com",
-		port : "6379",
-	});
-	cache.on('error', err => console.log('Redis Client Error', err));
-	console.log('Redis connected.');
 	connection.connect();
-
-	return [connection, cache];
+	return connection;
 }
